7bc3e276e1e0583e8841413992949ed1972316c4
squarepancakes <goh.shuyun@gmail.com>
2019-10-14 18:23:07 +0800

    Update dbURL

--- db.js
+++ db.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const dbName = "kittens-db";
 
 let dbURL;
-if (process.env.NODE_ENV === "development") {
-	dbURL = `mongodb://localhost/${dbName}`;
-}
+// if (process.env.NODE_ENV === "development") {
+// 	dbURL = `mongodb://localhost/${dbName}`;
+// }
 
-if (process.env.NODE.ENV === "production") {
-	dbURL = process.env.MONGO_URI;
-	console.log("MONGO_URI", process.env.MONGO_URI);
-	console.log("dbUrl", dbURL);
-}
+// if (process.env.NODE.ENV === "production") {
+dbURL = process.env.MONGO_URI;
+console.log("MONGO_URI", process.env.MONGO_URI);
+console.log("dbUrl", dbURL);
+// }
 
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
