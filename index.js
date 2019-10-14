f0dee78e6309533e3d065a04007376458f586b9e
squarepancakes <goh.shuyun@gmail.com>
2019-10-14 18:28:40 +0800

    Add port number for production

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const app = require("./app");
 // process.env.NODE_ENV = app.get("env")
 const env = app.get("env");
 
-let port;
-if (app.get("env") === "development") {
-	port = 5000;
-}
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
 	console.log(`Listening on ${port} in ${env} mode`);
